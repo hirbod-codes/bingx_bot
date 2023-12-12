6c8fd0920e54b325a39fcc7a022038687de4eab0
Hirbod Khatami <hirbod.khatami@gmail.com>
2023-12-12 19:31:36 +0000

    fix: remove useless method TradeOperations1 from Program class

--- src/bot/Program.cs
+++ src/bot/Program.cs
@@ -3,8 +3,6 @@ using Microsoft.Extensions.Configuration;
 using Serilog;
 using Serilog.Core;
 using Serilog.Settings.Configuration;
-using broker_api.src;
-using broker_api.src.Providers;
 
 namespace bot;
 
@@ -29,98 +27,5 @@ public class Program
             BotFactory.CreateBot().Run().Wait();
         }
         finally { Logger?.Dispose(); }
-    }
-
-    private static async Task TradeOperations1()
-    {
-        System.Console.WriteLine("\n\nTrade Operations begins...");
-
-        BingxUtilities utilities = new(Logger);
-
-        bool isLong = false;
-        System.Console.WriteLine("It's a " + (isLong ? "Long" : "Short") + " trade");
-
-        string strategyName = Configuration["StrategyName"]!;
-
-        float lastPrice = await new Market(Configuration[$"{strategyName}:BingxApi:BaseUri"]!, Configuration[$"{strategyName}:BingxApi:ApiKey"]!, Configuration[$"{strategyName}:BingxApi:ApiSecret"]!, Configuration[$"{strategyName}:BingxApi:Symbol"]!, utilities).GetLastPrice(Configuration[$"{strategyName}:BingxApi:Symbol"]!, int.Parse(Configuration[$"{strategyName}:BingxApi:TimeFrame"]!));
-        System.Console.WriteLine($"Last price is: {lastPrice}");
-
-        float quantity = (float)(float.Parse(Configuration[$"{strategyName}:BingxApi:Margin"]!) * int.Parse(Configuration[$"{strategyName}:BingxApi:Leverage"]!) / lastPrice);
-        System.Console.WriteLine($"Quantity is: {quantity}");
-
-        float sl = Trade.CalculateSl(isLong, 10, lastPrice, int.Parse(Configuration[$"{strategyName}:BingxApi:Leverage"]!));
-        System.Console.WriteLine($"SL is: {sl}");
-
-        float tp = Trade.CalculateTp(isLong, 10, lastPrice, int.Parse(Configuration[$"{strategyName}:BingxApi:Leverage"]!));
-        System.Console.WriteLine($"TP is: {tp}");
-
-        HttpResponseMessage response;
-
-        System.Console.WriteLine("GET_BALANCE");
-        response = await new Account(Configuration[$"{strategyName}:BingxApi:BaseUrl"]!, Configuration[$"{strategyName}:BingxApi:ApiKey"]!, Configuration[$"{strategyName}:BingxApi:ApiSecret"]!, Configuration[$"{strategyName}:BingxApi:Symbol"]!, utilities).GetBalance();
-        await utilities.EnsureSuccessfulBingxResponse(response);
-        System.Console.WriteLine("");
-
-        // System.Console.WriteLine("GET_LEVERAGE");
-        // response = await new Trade(Configuration[$"{strategyName}:BingxApi:BaseUrl"]!, Configuration[$"{strategyName}:BingxApi:ApiKey"]!, Configuration[$"{strategyName}:BingxApi:ApiSecret"]!, Configuration[$"{strategyName}:BingxApi:Symbol"]!).GetLeverage();
-        // await utilities.HandleBingxResponse(response);
-        // System.Console.WriteLine("");
-
-        // System.Console.WriteLine("SET_LEVERAGE");
-        // response = await new Trade(Configuration[$"{strategyName}:BingxApi:BaseUrl"]!, Configuration[$"{strategyName}:BingxApi:ApiKey"]!, Configuration[$"{strategyName}:BingxApi:ApiSecret"]!, Configuration[$"{strategyName}:BingxApi:Symbol"]!).SetLeverage(int.Parse(Configuration[$"{strategyName}:BingxApi:Leverage"]!), true);
-        // await utilities.HandleBingxResponse(response);
-        // System.Console.WriteLine("");
-
-        // System.Console.WriteLine("SET_LEVERAGE");
-        // response = await new Trade(Configuration[$"{strategyName}:BingxApi:BaseUrl"]!, Configuration[$"{strategyName}:BingxApi:ApiKey"]!, Configuration[$"{strategyName}:BingxApi:ApiSecret"]!, Configuration[$"{strategyName}:BingxApi:Symbol"]!).SetLeverage(int.Parse(Configuration[$"{strategyName}:BingxApi:Leverage"]!), false);
-        // await utilities.HandleBingxResponse(response);
-        // System.Console.WriteLine("");
-
-        // System.Console.WriteLine("GET_LEVERAGE");
-        // response = await new Trade(Configuration[$"{strategyName}:BingxApi:BaseUrl"]!, Configuration[$"{strategyName}:BingxApi:ApiKey"]!, Configuration[$"{strategyName}:BingxApi:ApiSecret"]!, Configuration[$"{strategyName}:BingxApi:Symbol"]!).GetLeverage();
-        // await utilities.HandleBingxResponse(response);
-        // System.Console.WriteLine("");
-
-        // System.Console.WriteLine("DELETE_ORDERS");
-        // response = await new Trade(Configuration[$"{strategyName}:BingxApi:BaseUrl"]!, Configuration[$"{strategyName}:BingxApi:ApiKey"]!, Configuration[$"{strategyName}:BingxApi:ApiSecret"]!, Configuration[$"{strategyName}:BingxApi:Symbol"]!).CloseOrders();
-        // await utilities.HandleBingxResponse(response);
-        // System.Console.WriteLine("");
-
-        // System.Console.WriteLine("CLOSE_OPEN_POSITIONS");
-        // response = await new Trade(Configuration[$"{strategyName}:BingxApi:BaseUrl"]!, Configuration[$"{strategyName}:BingxApi:ApiKey"]!, Configuration[$"{strategyName}:BingxApi:ApiSecret"]!, Configuration[$"{strategyName}:BingxApi:Symbol"]!).CloseOpenPositions();
-        // await utilities.HandleBingxResponse(response);
-        // System.Console.WriteLine("");
-
-        // System.Console.WriteLine("OPEN_MARKET_ORDER");
-        // response = await new Trade(Configuration[$"{strategyName}:BingxApi:BaseUrl"]!, Configuration[$"{strategyName}:BingxApi:ApiKey"]!, Configuration[$"{strategyName}:BingxApi:ApiSecret"]!, Configuration[$"{strategyName}:BingxApi:Symbol"]!).OpenMarketOrder(isLong, quantity, tp, sl);
-        // await utilities.HandleBingxResponse(response);
-        // System.Console.WriteLine("");
-
-        // System.Console.WriteLine("OPEN_LIMIT_ORDER");
-        // response = await new Trade(Configuration[$"{strategyName}:BingxApi:BaseUrl"]!, Configuration[$"{strategyName}:BingxApi:ApiKey"]!, Configuration[$"{strategyName}:BingxApi:ApiSecret"]!, Configuration[$"{strategyName}:BingxApi:Symbol"]!).OpenLimitOrder(isLong, quantity, lastPrice, tp, sl);
-        // await utilities.HandleResponse(response);
-        // System.Console.WriteLine("");
-
-        // System.Console.WriteLine("OPEN_TRIGGER_MARKET_ORDER");
-        // response = await new Trade(Configuration[$"{strategyName}:BingxApi:BaseUrl"]!, Configuration[$"{strategyName}:BingxApi:ApiKey"]!, Configuration[$"{strategyName}:BingxApi:ApiSecret"]!, Configuration[$"{strategyName}:BingxApi:Symbol"]!).OpenTriggerMarketOrder(isLong, quantity, 0.07785f, tp, sl);
-        // await utilities.HandleResponse(response);
-        // System.Console.WriteLine("");
-
-        // System.Console.WriteLine("OPEN_TRIGGER_LIMIT_ORDER");
-        // response = await new Trade(Configuration[$"{strategyName}:BingxApi:BaseUrl"]!, Configuration[$"{strategyName}:BingxApi:ApiKey"]!, Configuration[$"{strategyName}:BingxApi:ApiSecret"]!, Configuration[$"{strategyName}:BingxApi:Symbol"]!).OpenTriggerLimitOrder(isLong, quantity, lastPrice, 0.07546f, tp, sl);
-        // await utilities.HandleResponse(response);
-        // System.Console.WriteLine("");
-
-        System.Console.WriteLine("GET_ORDERS");
-        response = await new Trade(Configuration[$"{strategyName}:BingxApi:BaseUrl"]!, Configuration[$"{strategyName}:BingxApi:ApiKey"]!, Configuration[$"{strategyName}:BingxApi:ApiSecret"]!, Configuration[$"{strategyName}:BingxApi:Symbol"]!, utilities).GetOrders(new DateTime(2023, 12, 1, 22, 15, 0, DateTimeKind.Utc), DateTime.UtcNow);
-        await utilities.EnsureSuccessfulBingxResponse(response);
-        System.Console.WriteLine("");
-
-        // System.Console.WriteLine("GET_ORDERS");
-        // response = await new Trade(Configuration[$"{strategyName}:BingxApi:BaseUrl"]!, Configuration[$"{strategyName}:BingxApi:ApiKey"]!, Configuration[$"{strategyName}:BingxApi:ApiSecret"]!, Configuration[$"{strategyName}:BingxApi:Symbol"]!).GetOrders();
-        // await utilities.HandleBingxResponse(response);
-        // System.Console.WriteLine("");
-
-        System.Console.WriteLine("\n\nTrade Operations finished...");
     }
 }
