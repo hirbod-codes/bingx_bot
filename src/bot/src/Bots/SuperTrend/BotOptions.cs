7fbd44736daf6eda9c7150501adea1a7ca6c0e96
Hirbod Khatami <hirbod.khatami@gmail.com>
2024-04-03 18:50:36 +0000

    Update BotOptions.cs

--- src/bot/src/Bots/SuperTrend/BotOptions.cs
+++ src/bot/src/Bots/SuperTrend/BotOptions.cs
@@ -1,9 +1,11 @@
+using bot.src.Strategies;
+
 namespace bot.src.Bots.SuperTrendV1;
 
 public class BotOptions : IBotOptions
 {
-    public string Provider { get; set; } = null!;
-    public int TimeFrame { get; set; }
+    public string Provider { get; set; } = StrategyNames.SUPER_TREND_V1;
+    public int TimeFrame { get; set; } = 3600;
     public bool ShouldSkipOnParallelPositionRequest { get; internal set; } = false;
     public int RetryCount { get; internal set; } = 3;
 }
