107813c51227e480958a34b2e0297d5a3d54da20
Hirbod Khatami <hirbod.khatami@gmail.com>
2024-04-03 18:50:45 +0000

    Update IndicatorOptions.cs

--- src/bot/src/Indicators/SuperTrendV1/IndicatorOptions.cs
+++ src/bot/src/Indicators/SuperTrendV1/IndicatorOptions.cs
@@ -5,6 +5,6 @@ namespace bot.src.Indicators.SuperTrendV1;
 public class IndicatorOptions : IIndicatorOptions
 {
     public AtrOptions Atr { get; set; } = new() { Period = 14, Source = "close" };
-    public double AtrMultiplier { get; set; } = 4;
-    public SuperTrendV1Options SuperTrendOptions { get; set; } = new() { Multiplier = 1.5, Period = 30, CandlePart = CandlePart.HLCC4, ChangeATRCalculationMethod = true };
+    public double AtrMultiplier { get; set; } = 2;
+    public SuperTrendV1Options SuperTrendOptions { get; set; } = new() { Multiplier = 3.2, Period = 30, CandlePart = CandlePart.HLCC4, ChangeATRCalculationMethod = true };
 }
