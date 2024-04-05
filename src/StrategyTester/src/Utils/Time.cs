a98525e1d5332a14d56f12ce8adc3bcb6b4e6651
Hirbod Khatami <hirbod.khatami@gmail.com>
2024-04-05 06:37:06 +0000

    Update Time.cs

--- src/StrategyTester/src/Utils/Time.cs
+++ src/StrategyTester/src/Utils/Time.cs
@@ -17,7 +17,7 @@ public class Time : ITime
         throw new NotImplementedException();
     }
 
-    public Task StartTimer(int interval, ElapsedEventHandler elapsedEventHandler)
+    public Task<Timer> StartTimer(int interval, ElapsedEventHandler elapsedEventHandler)
     {
         throw new NotImplementedException();
     }
